0b5960a4776524370a0fd5d8713f20667776696d
nazariiturchynovych <nazariiturchynovych@gmail.com>
2024-06-15 16:14:27 +0300

    add docker compose and postgresql

--- src/Services/Catalog/Catalog.API/Products/CreateProductEndpoint.cs
+++
@@ -1,7 +0,0 @@
-namespace Catalog.API.Products;
-
-
-public class CreateProductEndpoint
-{
-    
-}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,8 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config => { config.RegisterServicesFromAssembly(typeof(Program).Assembly); });
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);    
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-
+app.MapCarter();
 
 app.Run();
