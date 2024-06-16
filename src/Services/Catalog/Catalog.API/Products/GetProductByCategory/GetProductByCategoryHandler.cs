15688ea3762165d3c07e617c4399a7adcbb92252
nazariiturchynovych <nazariiturchynovych@gmail.com>
2024-06-16 10:33:57 +0300

    Add GetProductById & Category handlers

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+internal class GetProductByCategoryQueryHandler
+    (IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+{
+    public ILogger<GetProductByCategoryQueryHandler> Logger { get; } = logger;
+
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        Logger.LogInformation("GetProductByCategoryQueryHandler.Called with {@query}", query);
+        
+        var products = await session.Query<Product>()
+            .Where(p => p.Category.Contains(query.Category))
+            .ToListAsync(cancellationToken);
+
+        return new GetProductByCategoryResult(products);
+    }
+}
--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -6,11 +6,15 @@ public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
 public record GetProductByIdResult(Product Product);
 
 internal class GetProductByIdQueryHandler
-    (IDocumentSession session)
+    (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
     : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
 {
+    public ILogger<GetProductByIdQueryHandler> Logger { get; } = logger;
+
     public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
     {
+        Logger.LogInformation("GetProductByIdQueryHandler.Called with {@query}", query);
+        
         var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
 
         if (product is null)
