4c9c3261dd1e22ce64bd5f30e3c927a8f1a788f4
nazariiturchynovych <nazariiturchynovych@gmail.com>
2024-06-17 12:46:18 +0300

    register caching decorator for basketRepo, add healthcheck for redis

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -21,15 +21,25 @@ builder.Services.AddMarten(opts =>
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
 
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    //options.InstanceName = "Basket";
+});
+
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 builder.Services.AddHealthChecks()
-    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 
+app.UseExceptionHandler(_ => { });
+
 app.MapCarter();
-app.UseExceptionHandler(options => { });
+
 app.UseHealthChecks("/health",
     new HealthCheckOptions
     {
