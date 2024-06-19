38f6573fe98fb05daa57b71ed17deeaf04c844c5
nazariiturchynovych <nazariiturchynovych@gmail.com>
2024-06-19 14:46:55 +0300

    add dbcontext, fix dependency injection

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,7 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddApplicationServices(builder.Configuration)
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseApiServices();
 
 app.Run();
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Ordering.Infrastructure.Data;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        : base(options) { }
+    
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+}
