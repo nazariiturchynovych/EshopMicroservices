6d932d1673a4770fa334596a3eaee16b6bb5746b
nazariiturchynovych <nazariiturchynovych@gmail.com>
2024-06-19 14:39:14 +0300

    Add project structure layers and their dependencies

---
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+using BuildingBlocks.Behaviors;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Ordering.Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+        
+        return services;
+    }
+}
