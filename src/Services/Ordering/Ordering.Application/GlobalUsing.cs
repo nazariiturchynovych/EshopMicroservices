47374f62b3f5467014bbdd9041ed4917ef92e0b9
nazariiturchynovych <nazariiturchynovych@gmail.com>
2024-06-23 10:23:02 +0300

    Add Order CUD commands

---
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -0,0 +1,8 @@
+﻿global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+global using BuildingBlocks.CQRS;
+global using Ordering.Application.Data;
+global using Ordering.Application.Dtos;
+global using FluentValidation;
+global using Microsoft.EntityFrameworkCore;
+global using Ordering.Application.Exceptions;
